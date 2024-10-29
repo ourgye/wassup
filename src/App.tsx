99e1f6a7d65ebdf9a61fc2058015a994bbc2a039
gye <cadmium4488@gmail.com>
2024-10-29 18:03:23 +0900

    feat: apply design

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,28 @@ function App() {
   const [messageList, setMessageList] = useState<string[]>(["안녕하세요"]);
   return (
     <>
-      <div className="card">
-        {messageList.map((m: string) => (
-          <SpeechBubble message={m} />
-        ))}
-        <form className="text-sm bg-gray-400 flex flex-row gap-1">
+      <div className="w-full h-full flex flex-col justify-between">
+        <div>
+          {messageList.map((m: string) => (
+            <SpeechBubble message={m} />
+          ))}
+        </div>
+        <form
+          className="text-sm bg-gray-400 flex flex-row gap-2 w-full px-2 py-2"
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (message) setMessageList([...messageList, message]);
+            setMessage("");
+          }}
+        >
           <input
             id="message"
+            className="w-full px-2 rounded-md"
             value={message}
             placeholder="입력하세요"
             onChange={(e) => setMessage(e.target.value)}
           />
-          <button
-            type="button"
-            onClick={() => {
-              if (message) setMessageList([...messageList, message]);
-              setMessage("");
-            }}
-            className="px-2 py-0"
-          >
+          <button type="submit" className="px-2 py-0.5 line break-keep">
             입력
           </button>
         </form>
--- src/components/SpeechBubble.tsx
+++ src/components/SpeechBubble.tsx
@@ -1,6 +1,6 @@
 function SpeechBubble({ message }: { message: string }) {
   return (
-    <div>
+    <div className="max-w-fit">
       <div className="relative px-4 h-8 bg-red-300 text-sm text-center flex justify-center items-center rounded-md mb-[0.5rem]">
         <span>{message}</span>
         <div className="absolute w-0 h-0 right-1/2 -bottom-1/2 translate-x-1/2 border-transparent border-[0.5rem] border-t-red-300" />
